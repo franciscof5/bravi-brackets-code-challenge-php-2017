c01b0e9bc830a60c7da51e4e1a96a2548a96e3b4
Francisco Mat <francisco@f5sites.com>
2018-05-08 19:06:51 -0300

    initial structure

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+var_dump($_GET);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bravi Brackets</title>
+</head>
+<body>
+<form method="GET" name="b1" action="index.php">
+	<input type="text" name="b1-b">
+	<input type="submit" name="b1-s" value="verify">
+</form>
+</body>
+</html>
