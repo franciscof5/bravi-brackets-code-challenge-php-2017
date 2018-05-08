460cc5c8822ba3bc52b4bc10cdaa83c7aacb7891
Francisco Mat <francisco@f5sites.com>
2018-05-08 19:19:43 -0300

    license and author information

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ivarb-brackets
-Ibarb Brackets Parser
+Ivarb Brackets Parser
+
+Insert the brackets in the input field and submit it to itself ($_GET) to check brackets validations
+
+Author: Francisco Matelli Matulovic
--- index.php
+++ index.php
@@ -7,9 +7,13 @@ var_dump($_GET);
 	<title>Bravi Brackets</title>
 </head>
 <body>
+<h1>Brackets parser</h1>
+<p>Write strings with brackets above to check out consistency</p>
 <form method="GET" name="b1" action="index.php">
 	<input type="text" name="b1-b">
 	<input type="submit" name="b1-s" value="verify">
 </form>
+<p>Author: Francisco Matelli Matulovic</p>
+<p>Released under GNU GPLv3</p>
 </body>
 </html>
